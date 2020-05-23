4b8348cdefe9f74139f7bf9b71dbcab101b091e5
Gökhan <Gkhan@LAPTOP-N3APN9PF>
2020-05-24 00:30:12 +0300

    Muhasebeci entity sınıfının içi dolduruldu

--- src/java/entity/Muhasebeci.java
+++ src/java/entity/Muhasebeci.java
@@ -1,15 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package entity;
+
 /**
  *
  * @author gkhnya
  */
 
 public class Muhasebeci {
+
+    private Long muhasebeciId;
+    private String muhasebeciAd;
+    private String muhasebeciSoyad;
+    private String muhasebeciTc;
+
+    public Muhasebeci() {
+    }
+
+    public Muhasebeci(Long muhasebeciId, String muhasebeciAd, String muhasebeciSoyad, String muhasebeciTc) {
+        this.muhasebeciId = muhasebeciId;
+        this.muhasebeciAd = muhasebeciAd;
+        this.muhasebeciSoyad = muhasebeciSoyad;
+        this.muhasebeciTc = muhasebeciTc;
+    }
+
+    public Long getMuhasebeciId() {
+        return muhasebeciId;
+    }
+
+    public void setMuhasebeciId(Long muhasebeciId) {
+        this.muhasebeciId = muhasebeciId;
+    }
+
+    public String getMuhasebeciAd() {
+        return muhasebeciAd;
+    }
+
+    public void setMuhasebeciAd(String muhasebeciAd) {
+        this.muhasebeciAd = muhasebeciAd;
+    }
+
+    public String getMuhasebeciSoyad() {
+        return muhasebeciSoyad;
+    }
+
+    public void setMuhasebeciSoyad(String muhasebeciSoyad) {
+        this.muhasebeciSoyad = muhasebeciSoyad;
+    }
+
+    public String getMuhasebeciTc() {
+        return muhasebeciTc;
+    }
+
+    public void setMuhasebeciTc(String muhasebeciTc) {
+        this.muhasebeciTc = muhasebeciTc;
+    }
 
 }
