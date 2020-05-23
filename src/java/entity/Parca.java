3c61dd33a9cb14be4d3129d653eb0f15916cad4d
Gökhan <Gkhan@LAPTOP-N3APN9PF>
2020-05-24 00:37:20 +0300

    Parça entity sınıfı düzenlendi

--- src/java/entity/Parca.java
+++ src/java/entity/Parca.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
 /**
  *
  * @author H Cem ERYILMAZ
  */
+
 public class Parca {
     
 }
