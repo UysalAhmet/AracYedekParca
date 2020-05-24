4a011d5d8b4b9fd1ef01ca139b39c1fe884f9daa
Ahmet <56485417+UysalAhmet@users.noreply.github.com>
2020-05-24 19:15:08 +0300

    Parca ve Siparis classları dolduruldu

--- src/java/entity/Parca.java
+++ src/java/entity/Parca.java
@@ -6,5 +6,63 @@ package entity;
  */
 
 public class Parca {
+     private Long parcaId;
+    private String parcaAd;
+    private String parcaHammadde;
+    private Makina makina;
+    private Long parcaFiyat;
+
+    public Parca() {
+    }
+
+    public Parca(Long parcaId, String parcaAd, String parcaHammadde, Makina makinaId, Long parcaFiyat) {
+        this.parcaId = parcaId;
+        this.parcaAd = parcaAd;
+        this.parcaHammadde = parcaHammadde;
+        this.makina = makina;
+        this.parcaFiyat = parcaFiyat;
+    }
+
+    public Long getParcaId() {
+        return parcaId;
+    }
+
+    public void setParcaId(Long parcaId) {
+        this.parcaId = parcaId;
+    }
+
+    public String getParcaAd() {
+        return parcaAd;
+    }
+
+    public void setParcaAd(String parcaAd) {
+        this.parcaAd = parcaAd;
+    }
+
+    public String getParcaHammadde() {
+        return parcaHammadde;
+    }
+
+    public void setParcaHammadde(String parcaHammadde) {
+        this.parcaHammadde = parcaHammadde;
+    }
+
+    public Makina getMakina() {
+        return makina;
+    }
+
+    public void setMakina(Makina makina) {
+        this.makina = makina;
+    }
+
+   
+
+    public Long getParcaFiyat() {
+        return parcaFiyat;
+    }
+
+    public void setParcaFiyat(Long parcaFiyat) {
+        this.parcaFiyat = parcaFiyat;
+    }
     
 }
--- src/java/entity/Siparis.java
+++ src/java/entity/Siparis.java
@@ -5,10 +5,78 @@
  */
 package entity;
 
+import java.util.List;
+
 /**
  *
  * @author H Cem ERYILMAZ
  */
 public class Siparis {
-    
+     private Long siparisId;
+    private String siparisKod;
+    private Long siparisParcaSayi;
+    private Parca parca;
+    private Muhasebeci muhasebeci;
+    private List<Firma> siparisVerenFirmalar;
+
+    public Siparis() {
+    }
+
+    public Siparis(Long siparisId, String siparisKod, Long siparisParcaSayi, Parca parca, Muhasebeci muhasebeci) {
+        this.siparisId = siparisId;
+        this.siparisKod = siparisKod;
+        this.siparisParcaSayi = siparisParcaSayi;
+        this.parca = parca;
+        this.muhasebeci = muhasebeci;
+    }
+
+
+    public Long getSiparisId() {
+        return siparisId;
+    }
+
+    public void setSiparisId(Long siparisId) {
+        this.siparisId = siparisId;
+    }
+
+    public String getSiparisKod() {
+        return siparisKod;
+    }
+
+    public void setSiparisKod(String siparisKod) {
+        this.siparisKod = siparisKod;
+    }
+
+    public Long getSiparisParcaSayi() {
+        return siparisParcaSayi;
+    }
+
+    public void setSiparisParcaSayi(Long siparisParcaSayi) {
+        this.siparisParcaSayi = siparisParcaSayi;
+    }
+
+    public Parca getParca() {
+        return parca;
+    }
+
+    public void setParca(Parca parca) {
+        this.parca = parca;
+    }
+
+    public Muhasebeci getMuhasebeci() {
+        return muhasebeci;
+    }
+
+    public void setMuhasebeci(Muhasebeci muhasebeci) {
+        this.muhasebeci = muhasebeci;
+    }
+
+    public List<Firma> getSiparisVerenFirmalar() {
+        return siparisVerenFirmalar;
+    }
+
+    public void setSiparisVerenFirmalar(List<Firma> siparisVerenFirmalar) {
+        this.siparisVerenFirmalar = siparisVerenFirmalar;
+    }
+
 }
