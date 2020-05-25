86f4fa9097a45bc06dcc5b1d1096dff770cfb999
H Cem ERYILMAZ <H Cem ERYILMAZ@192.168.1.50>
2020-05-26 00:48:49 +0300

    Usta ve isci daolar yazıldı

---
+++ src/java/dao/IsciDAO.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author H Cem ERYILMAZ
+ */
+import entity.Isci;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import util.DbConnection;
+
+public class IsciDAO {
+    private DbConnection db;
+    private Connection c;
+    private UstaDAO ustaDao;
+
+    public List<Isci> getIsciList() {
+        List<Isci> isciDaoList = new ArrayList();
+        try {
+            PreparedStatement pst = this.getC().prepareStatement("select * from isci");
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                Isci tmp = new Isci();
+                tmp.setIsciId(rs.getLong("isci_id"));
+                tmp.setIsciAd(rs.getString("isci_ad"));
+                tmp.setIsciSoyad(rs.getString("isci_soyad"));
+                tmp.setIsciTc(rs.getString("isci_tc"));
+                tmp.setUsta(this.getUstaDao().ustaIdBul(rs.getLong("usta_id")));
+
+                isciDaoList.add(tmp);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return isciDaoList;
+    }
+
+    public void insert(Isci isci, Long selectedIsci) {
+
+        try {
+            PreparedStatement pst = this.getC().prepareStatement("insert into isci "
+                    + "(isci_ad,isci_soyad,isci_tc,usta_id) values(?,?,?,?)");
+            pst.setString(1, isci.getIsciAd());
+            pst.setString(2, isci.getIsciSoyad());
+            pst.setString(3, isci.getIsciTc());
+            pst.setLong(4, selectedIsci);
+
+            pst.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void delete(Isci isci) {
+
+        try {
+            PreparedStatement pst = this.getC().prepareStatement("delete from isci where isci_id=?");
+            pst.setLong(1, isci.getIsciId());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void update(Isci isci, Long selectedIsci) {
+
+        try {
+            PreparedStatement pst = this.getC().prepareStatement("update isci set isci_ad=? ,isci_soyad=?, isci_tc=?, isci_id=?   where isci_id =?");
+            pst.setString(1, isci.getIsciAd());
+            pst.setString(2, isci.getIsciSoyad());
+            pst.setString(3, isci.getIsciTc());
+            pst.setLong(4, selectedIsci);
+            pst.setLong(5, isci.getIsciId());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public Isci isciIdBul(long isciId) {
+        Isci isci = null;
+
+        try {
+
+            PreparedStatement pst = this.getC().prepareStatement("select * from isci where isci_id=" + isciId);
+            ResultSet rs = pst.executeQuery();
+            rs.next();
+            isci = new Isci();
+            isci.setIsciId(rs.getLong("isci_id"));
+            isci.setIsciAd(rs.getString("isci_ad"));
+            isci.setIsciSoyad(rs.getString("isci_soyad"));
+            isci.setIsciTc(rs.getString("isci_tc"));
+            isci.setUsta(this.getUstaDao().ustaIdBul(rs.getLong("isci_id")));
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return isci;
+    }
+
+    public DbConnection getDb() {
+        if (this.db == null) {
+            this.db = new DbConnection();
+        }
+        return db;
+    }
+
+    public void setDb(DbConnection db) {
+        this.db = db;
+    }
+
+    public Connection getC() {
+        if (this.c == null) {
+            this.c = getDb().connect();
+        }
+        return c;
+    }
+
+    public void setC(Connection c) {
+        this.c = c;
+    }
+
+    public UstaDAO getUstaDao() {
+        if (this.ustaDao == null) {
+            this.ustaDao = new UstaDAO();
+        }
+        return ustaDao;
+    }
+}
---
+++ src/java/dao/UstaDAO.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author H Cem ERYILMAZ
+ */
+import entity.Usta;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import util.DbConnection;
+public class UstaDAO {
+     private DbConnection db;
+    private Connection c;
+
+  
+    
+    public List<Usta> getUstaList() {
+         List<Usta> ustaDaoList = new ArrayList();
+        try {
+            PreparedStatement pst = this.getC().prepareStatement("select * from usta");
+            ResultSet rs = pst.executeQuery();
+            
+            while(rs.next()){
+                Usta tmp = new Usta();
+                tmp.setUstaId(rs.getLong("usta_id"));
+                tmp.setUstaAd(rs.getString("usta_ad"));
+                tmp.setUstaSoyad(rs.getString("usta_soyad"));
+                tmp.setUstaTc(rs.getString("usta_tc"));
+               
+                
+                ustaDaoList.add(tmp);
+            }
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return ustaDaoList;
+    }
+
+
+    public void insert(Usta usta) {
+      
+    try{
+            PreparedStatement pst = this.getC().prepareStatement("insert into usta "
+                    + "(usta_ad,usta_soyad,usta_tc) values(?,?,?)");
+            pst.setString(1, usta.getUstaAd());
+            pst.setString(2, usta.getUstaSoyad());
+            pst.setString(3, usta.getUstaTc());
+            
+            pst.executeUpdate();
+           
+        } catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+
+    public void delete(Usta usta) {
+    
+         try{
+            PreparedStatement pst = this.getC().prepareStatement("delete from usta where usta_id=?");
+            pst.setLong(1, usta.getUstaId());
+            pst.executeUpdate();
+            } catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+    
+    }
+
+    public void update(Usta usta) {
+    
+         try{
+            PreparedStatement pst = this.getC().prepareStatement("update usta set usta_ad=? ,usta_soyad=?, usta_tc=?   where usta_id =?");
+            pst.setString(1, usta.getUstaAd());
+            pst.setString(2, usta.getUstaSoyad());
+            pst.setString(3, usta.getUstaTc());
+            pst.setLong(4, usta.getUstaId());
+            pst.executeUpdate();
+            } catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    
+    }
+    
+    
+    public Usta ustaIdBul(Long ustaId){
+        Usta usta=null;
+        
+        try{
+            
+            PreparedStatement pst = this.getC().prepareStatement("select * from usta where usta_id="+ustaId);
+            ResultSet rs =pst.executeQuery();
+            rs.next();
+            usta=new Usta();
+            usta.setUstaId(rs.getLong("usta_id"));
+            usta.setUstaAd(rs.getString("usta_ad"));
+            usta.setUstaSoyad(rs.getString("usta_soyad"));
+            usta.setUstaTc(rs.getString("usta_tc"));
+            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
+        return usta;
+    }
+       
+    public DbConnection getDb() {
+        if(this.db==null){
+            this.db=new DbConnection();
+        }
+        return db;
+    }
+
+    public void setDb(DbConnection db) {
+        this.db = db;
+    }
+
+    public Connection getC() {
+        if(this.c==null){
+            this.c= getDb().connect();
+        }
+        return c;
+    }
+
+    public void setC(Connection c) {
+        this.c = c;
+    }
+    
+}
