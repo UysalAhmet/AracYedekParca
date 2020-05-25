a9610eb6fae2ebc1ee6f65e46e9fc63e7f42d9aa
Gökhan <Gkhan@LAPTOP-N3APN9PF>
2020-05-25 12:11:31 +0300

    Firma entity sınıfının içi dolduruldu

--- src/java/entity/Firma.java
+++ src/java/entity/Firma.java
@@ -6,5 +6,42 @@ package entity;
  */
 
 public class Firma {
-    
+
+    private Long firmaId;
+    private String firmaAd;
+    private String firmaAciklama;
+
+    public Firma() {
+    }
+
+    public Firma(Long firmaId, String firmaAd, String firmaAciklama) {
+        this.firmaId = firmaId;
+        this.firmaAd = firmaAd;
+        this.firmaAciklama = firmaAciklama;
+    }
+
+    public Long getFirmaId() {
+        return firmaId;
+    }
+
+    public void setFirmaId(Long firmaId) {
+        this.firmaId = firmaId;
+    }
+
+    public String getFirmaAd() {
+        return firmaAd;
+    }
+
+    public void setFirmaAd(String firmaAd) {
+        this.firmaAd = firmaAd;
+    }
+
+    public String getFirmaAciklama() {
+        return firmaAciklama;
+    }
+
+    public void setFirmaAciklama(String firmaAciklama) {
+        this.firmaAciklama = firmaAciklama;
+    }
+
 }
