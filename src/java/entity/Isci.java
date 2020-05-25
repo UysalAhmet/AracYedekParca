8bc27aca3c1b921574e993476eb45d3a4ff5a7c0
H Cem ERYILMAZ <H Cem ERYILMAZ@192.168.1.50>
2020-05-26 00:46:08 +0300

    Usta ve isci entityleri yazıldı

--- src/java/entity/Isci.java
+++ src/java/entity/Isci.java
@@ -6,5 +6,61 @@ package entity;
  */
 
 public class Isci {
-    
+     private Long isciId;
+    private String isciAd;
+    private String isciSoyad;
+    private String isciTc;
+    private Usta usta;
+
+    public Isci() {
+    }
+
+    public Isci(Long isciId, String isciAd, String isciSoyad, String isciTc, Usta usta) {
+        this.isciId = isciId;
+        this.isciAd = isciAd;
+        this.isciSoyad = isciSoyad;
+        this.isciTc = isciTc;
+        this.usta = usta;
+    }
+
+    public Long getIsciId() {
+        return isciId;
+    }
+
+    public void setIsciId(Long isciId) {
+        this.isciId = isciId;
+    }
+
+    public String getIsciAd() {
+        return isciAd;
+    }
+
+    public void setIsciAd(String isciAd) {
+        this.isciAd = isciAd;
+    }
+
+    public String getIsciSoyad() {
+        return isciSoyad;
+    }
+
+    public void setIsciSoyad(String isciSoyad) {
+        this.isciSoyad = isciSoyad;
+    }
+
+    public String getIsciTc() {
+        return isciTc;
+    }
+
+    public void setIsciTc(String isciTc) {
+        this.isciTc = isciTc;
+    }
+
+    public Usta getUsta() {
+        return usta;
+    }
+
+    public void setUsta(Usta usta) {
+        this.usta = usta;
+    }
+
 }
--- src/java/entity/Usta.java
+++ src/java/entity/Usta.java
@@ -6,5 +6,52 @@ package entity;
  */
 
 public class Usta {
+    private Long ustaId;
+    private String ustaAd;
+    private String ustaSoyad;
+    private String ustaTc;
+
+    public Usta() {
+    }
+
+    public Usta(Long ustaId, String ustaAd, String ustaSoyad, String ustaTc) {
+        this.ustaId = ustaId;
+        this.ustaAd = ustaAd;
+        this.ustaSoyad = ustaSoyad;
+        this.ustaTc = ustaTc;
+    }
+
+    public Long getUstaId() {
+        return ustaId;
+    }
+
+    public void setUstaId(Long ustaId) {
+        this.ustaId = ustaId;
+    }
+
+    public String getUstaAd() {
+        return ustaAd;
+    }
+
+    public void setUstaAd(String ustaAd) {
+        this.ustaAd = ustaAd;
+    }
+
+    public String getUstaSoyad() {
+        return ustaSoyad;
+    }
+
+    public void setUstaSoyad(String ustaSoyad) {
+        this.ustaSoyad = ustaSoyad;
+    }
+
+    public String getUstaTc() {
+        return ustaTc;
+    }
+
+    public void setUstaTc(String ustaTc) {
+        this.ustaTc = ustaTc;
+    }
+    
     
 }
