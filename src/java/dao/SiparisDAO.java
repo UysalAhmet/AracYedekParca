10d78ad446c0b5207ad74c788d9842082216dced
Gökhan <Gkhan@LAPTOP-N3APN9PF>
2020-05-26 18:52:36 +0300

    SiparisDAO sınıfı düzenlendi

--- src/java/dao/SiparisDAO.java
+++ src/java/dao/SiparisDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import entity.Siparis;
@@ -19,6 +14,7 @@ import util.DbConnection;
  *
  * @author ahmet
  */
+
 public class SiparisDAO {
 
     private DbConnection db;
