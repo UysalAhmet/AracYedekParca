b5799b34dde0ff81dc2583efe5678248ea543bbc
H Cem ERYILMAZ <H Cem ERYILMAZ@192.168.1.50>
2020-05-28 15:36:47 +0300

    Usta ve isci controller sınıfı düzenlendi.

--- src/java/controller/UstaController.java
+++ src/java/controller/UstaController.java
@@ -9,6 +9,77 @@ package controller;
  *
  * @author H Cem ERYILMAZ
  */
-public class UstaController {
+import dao.UstaDAO;
+import entity.Usta;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+@Named
+@SessionScoped
+public class UstaController implements Serializable {
+    private List<Usta> ustaControllerList;
+    private UstaDAO ustadao;
+    private Usta usta;
+
+    
+    
+    public void updateForm(Usta usta){
+        this.usta=usta;
+        
+    }
+    
+    public void update(){
+        this.getUstadao().update(this.usta);
+        this.usta= new Usta();
+    }
+    
+    
+    public void delete(Usta usta){
+        this.getUstadao().delete(usta);
+        this.usta= new Usta();
+    }
+    
+    
+    public void clearForm(){
+        this.usta= new Usta();
+    }
+    
+    public void create(){
+        
+        this.getUstadao().insert(this.usta);
+        
+        this.usta= new Usta();
+    }
+
+    public List<Usta> getUstaControllerList() {
+        this.ustaControllerList=this.getUstadao().getUstaList();
+        return ustaControllerList;
+    }
+
+    public void setUstaControllerList(List<Usta> ustaControllerList) {
+        this.ustaControllerList = ustaControllerList;
+    }
+
+    public UstaDAO getUstadao() {
+        if(this.ustadao==null)
+            this.ustadao = new UstaDAO();
+        return ustadao;
+    }
+
+    public void setUstadao(UstaDAO ustadao) {
+        this.ustadao = ustadao;
+    }
+
+    public Usta getUsta() {
+        if(this.usta==null)
+            this.usta = new Usta();
+        return usta;
+    }
+
+    public void setUsta(Usta usta) {
+        this.usta = usta;
+    }
+    
     
 }
--- src/java/controller/İsciController.java
+++ src/java/controller/İsciController.java
@@ -9,6 +9,109 @@ package controller;
  *
  * @author H Cem ERYILMAZ
  */
-public class İsciController {
+import dao.IsciDAO;
+import dao.UstaDAO;
+import entity.Isci;
+import entity.Usta;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class İsciController implements Serializable {
+     private List<Isci> isciControllerList;
+    private IsciDAO iscidao;
+    private Isci isci;
+    private Long selectedUsta;
+    private UstaDAO ustaDao;
+    private  List<Usta> ustaList;
+
+
+    
+    public void updateForm(Isci isci){
+        this.isci=isci;
+        this.selectedUsta=this.isci.getUsta().getUstaId();
+        
+    }
+    
+    public void update(){
+        this.getIscidao().update(this.isci,selectedUsta);
+        this.isci= new Isci();
+    }
+    
+    
+    public void delete(Isci isci){
+        this.getIscidao().delete(isci);
+        this.isci= new Isci();
+    }
+    
+    
+    public void clearForm(){
+        this.isci= new Isci();
+    }
+    
+    public void create(){
+        
+        this.getIscidao().insert(this.isci,selectedUsta);
+        
+        this.isci= new Isci();
+    }
+
+    public List<Isci> getIsciControllerList() {
+        this.isciControllerList=this.getIscidao().getIsciList();
+        return isciControllerList;
+    }
+
+    public void setIsciControllerList(List<Isci> isciControllerList) {
+        this.isciControllerList = isciControllerList;
+    }
+
+    public IsciDAO getIscidao() {
+        if(this.iscidao==null)
+            this.iscidao = new IsciDAO();
+        return iscidao;
+    }
+
+    public void setIscidao(IsciDAO iscidao) {
+        this.iscidao = iscidao;
+    }
+
+    public Isci getIsci() {
+        if(this.isci==null)
+            this.isci = new Isci();
+        return isci;
+    }
+
+    public void setIsci(Isci isci) {
+        this.isci = isci;
+    }
+
+    public Long getSelectedUsta() {
+        return selectedUsta;
+    }
+
+    public void setSelectedUsta(Long selectedUsta) {
+        this.selectedUsta = selectedUsta;
+    }
+
+    public UstaDAO getUstaDao() {
+        if(this.ustaDao==null){
+            this.ustaDao= new UstaDAO();
+        }
+        return ustaDao;
+    }
+
+    public List<Usta> getUstaList() {
+        this.ustaList=this.getUstaDao().getUstaList();
+        return ustaList;
+    }
+
+    public void setUstaList(List<Usta> ustaList) {
+        this.ustaList = ustaList;
+    }
+    
+    
     
 }
