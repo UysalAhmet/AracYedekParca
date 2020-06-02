d63a2edd6d4edaf64d9a9c4b00f0819468cf1edd
Gökhan <Gkhan@LAPTOP-N3APN9PF>
2020-06-02 15:37:21 +0300

    MakinaSorumlusu entity sınıfı düzenlendi

--- src/java/entity/MakinaSorumlusu.java
+++ src/java/entity/MakinaSorumlusu.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package entity;
 
 import java.util.Objects;
@@ -11,6 +6,7 @@ import java.util.Objects;
  *
  * @author yasemin
  */
+
 public class MakinaSorumlusu {
    
     private Long makinasorumlusuId;
